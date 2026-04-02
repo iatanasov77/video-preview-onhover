9b4d6dc7c9365afde53955a3d2aca82051c20a64
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-04-02 11:26:16 +0000

    Add an Option.

--- video-preview-onhover.js
+++ video-preview-onhover.js
@@ -2,14 +2,19 @@ require( './video-preview-onhover.css' )
 
 /**
  * Expired from: https://github.com/Gyanreyer/hover-video-player
+ *
+ * options
+ * {
+ *     restartOnPause: true
+ * }
  */
 export function VideoPreviewOnhover( videoTargets, options = {} )
 {
     for ( const hoverTarget of videoTargets ) {
-        hoverTarget.addEventListener( "mouseenter", onHover );
-        hoverTarget.addEventListener( "mouseleave", onBlur );
-        hoverTarget.addEventListener( "focus", onHover );
-        hoverTarget.addEventListener( "blur", onBlur );
+        hoverTarget.addEventListener( "mouseenter", onHover, options );
+        hoverTarget.addEventListener( "mouseleave", onBlur, options );
+        hoverTarget.addEventListener( "focus", onHover, options );
+        hoverTarget.addEventListener( "blur", onBlur, options );
         hoverTarget.addEventListener( "touchstart", onHover, {
             passive: true,
         });
@@ -19,38 +24,46 @@ export function VideoPreviewOnhover( videoTargets, options = {} )
 /**
  * Handler for hover events on hover target
  */
-function onHover( event )
+function onHover( event, options )
 {
     const video = this.getElementsByTagName( 'video' )[0];
     const poster = this.getElementsByTagName( 'img' )[0];
+    
     startPreview( video, poster );
 }
 
 /**
  * Handler for blur events on hover target
  */
-function onBlur( event ) {
+function onBlur( event, options ) {
     const video = this.getElementsByTagName( 'video' )[0];
     const poster = this.getElementsByTagName( 'img' )[0];
-    stopPreview( video, poster );
+    
+    var restartOnPause = options.restartOnPause || false;
+    stopPreview( video, poster, restartOnPause );
 }
 
 function startPreview( video, poster )
 {
     video.muted = true;
-    video.currentTime = 1;
-    video.playbackRate = 0.5;
+//     video.currentTime = 1;
+//     video.playbackRate = 0.5;
     video.play();
     
     video.style.display = '';
     poster.style.display = 'none';
 }
 
-function stopPreview( video, poster )
+function stopPreview( video, poster, restartOnPause )
 {
-    video.currentTime = 0;
-    video.playbackRate = 1;
-    video.pause();
+//     video.currentTime = 0;
+//     video.playbackRate = 1;
+
+    if ( restartOnPause ) {
+        video.stop();
+    } else {
+        video.pause();
+    }
     
     video.style.display = 'none';
     poster.style.display = '';
